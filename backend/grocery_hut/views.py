04b84f6667e7123bfc944995fa819db060b52de4
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-17 12:04:15 +0200

    create a model view for the api data

--- backend/grocery_hut/views.py
+++ backend/grocery_hut/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from grocery_hut.serializers import GroceryHutSerializer
+from grocery_hut.models import GroceryHut
 
 # Create your views here.
+
+class GroceryHutView(viewsets.ModelViewSet):
+    serializer_class = GroceryHutSerializer
+    queryset = GroceryHut.objects.all()
+    
+'''
+The viewsets base class provides the implementation for CRUD operations by default.
+'''
