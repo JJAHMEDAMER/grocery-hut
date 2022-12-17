c9618c1048b06827f1c7736c1c69f0dcd0a3abad
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-17 11:42:46 +0200

    white list the react view url:port

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+## Our Frontend Live on port 3000 
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
