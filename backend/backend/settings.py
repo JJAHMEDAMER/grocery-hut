cdbc91ffd0631d9e45e615634025f1fecd856b4b
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-17 11:41:17 +0200

    add rest_framework & corsheaders as installed apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'grocery_hut'
+    'grocery_hut',
+    'rest_framework',  # dependencies
+    'corsheaders'  # dependencies
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
