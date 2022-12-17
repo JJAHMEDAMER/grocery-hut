24858e542230ef0d4bec3fc40b8a0d5517cbc220
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-17 11:35:03 +0200

    setup an admin page for grocery list model

--- backend/grocery_hut/admin.py
+++ backend/grocery_hut/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+# Models
+from grocery_hut.models import GroceryHut
+
 # Register your models here.
+
+class GroceryHutAdmin(admin.ModelAdmin):
+    list_display = ('title',)  # must be list so add , after string
+    
+admin.site.register(GroceryHut, GroceryHutAdmin)
