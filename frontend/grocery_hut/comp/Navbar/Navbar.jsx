2f4bb04b0c021dbda9e53512e3428bf6e2783c78
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-01-05 19:17:20 +0200

    static router next/link

--- frontend/grocery_hut/comp/Navbar/Navbar.jsx
+++ frontend/grocery_hut/comp/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import styles from "./Navbar.module.css"
 
+import Link from "next/link"
+
 export const Navbar = () => {
     return <nav className={styles.nav}>
         <h1 className={styles.app_title}>Grocery Hut</h1>
         <ul className={styles.list}>
-            <li>Login</li>
-            <li>SignUp</li>
+            <li><Link href="/login">Login</Link></li>
+            <li><Link href="/login">SignUp</Link></li>
         </ul>
     </nav>
 }
