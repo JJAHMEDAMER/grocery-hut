d49e01572a7559691f16f6ffffc4aa11dca61dd3
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-01-05 22:28:57 +0200

    capture user input in signup form

--- frontend/grocery_hut/pages/signup/index.jsx
+++ frontend/grocery_hut/pages/signup/index.jsx
@@ -1,24 +1,57 @@
 import Link from "next/link"
-import styles from "./signup.module.css"
+import { useState } from "react"
 
+//
+import styles from "./signup.module.css"
 import { Navbar } from "../../comp"
 
 const signup = () => {
+    const [username, setUsername] = useState('')  // the value can't be undefined or null
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    console.log({
+        username,
+        email,
+        password
+    })
+
     return <div>
         <Navbar />
         <div className={styles.form_container}>
             <form className={styles.form}>
                 <div className={styles.section}>
                     <label htmlFor="username" className={styles.label}>Username</label>
-                    <input className={styles.input} type="text" name="username" placeholder="Enter username" />
+                    <input
+                        className={styles.input}
+                        type="text"
+                        name="username"
+                        placeholder="Enter username"
+                        value={username}  // ...force the input's value to match the state variable...
+                        onChange={(event) => setUsername(event.target.value)}  // ... and update the state variable on any edits
+                    />
                 </div>
                 <div className={styles.section}>
                     <label htmlFor="email" className={styles.label}>Email</label>
-                    <input className={styles.input} type="email" name="email" placeholder="Enter your email" />
+                    <input
+                        className={styles.input}
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className={styles.section}>
                     <label htmlFor="password" className={styles.label}>Password</label>
-                    <input className={styles.input} type="password" name="password" placeholder="Enter your password" />
+                    <input
+                        className={styles.input}
+                        type="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                 </div>
                 <button className={styles.button} type="submit">Sign Up</button>
                 <p className={styles.no_account}>Already have an account? <Link href="/login">Login</Link></p>
